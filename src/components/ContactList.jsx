cd05435a04a8761200da0f68009fe174008312c7
joehobos <joehobos@DESKTOP-A520OBQ>
2023-06-26 03:11:14 -0700

    add api

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import ContactRow from "./ContactRow"
+import { useEffect } from "react";
 const dummyContacts = [
     { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
     { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
@@ -10,6 +11,20 @@ const dummyContacts = [
 
 function ContactList() {
     const [contacts, setContacts] = useState(dummyContacts)
+    useEffect(() => {
+        async function fetchContacts() {
+          try {
+            const response = await fetch(
+              "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
+            );
+            const result = await response.json();
+            setContacts(result);
+          } catch (error) {
+            console.error(error);
+          }
+        }
+        fetchContacts();
+      }, []);
     
     return (
         <table>
